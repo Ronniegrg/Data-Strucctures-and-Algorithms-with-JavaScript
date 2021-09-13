a866edac9986a08fd3587003a490caba16279e55
Rouni Gorgees <rounigorgees@gmail.com>
2021-09-12 21:58:37 -0400

    add link to complete study

--- Array-splice/remove-zero-element.js
+++ Array-splice/remove-zero-element.js
@@ -2,3 +2,4 @@ let myFish = [ 'angel', 'clown', 'mandarin', 'sturgeon' ];
 let remove = myFish.splice( 2, 0, 'drum' );
 console.log( myFish );
 console.log( remove );
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
