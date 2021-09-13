5f22a98f9b90a69d54f47326b3e66429839da61e
Rouni Gorgees <rounigorgees@gmail.com>
2021-09-12 21:23:10 -0400

    use concat() function allows to put together two or more arrays to create a new array.

---
+++ concat-function.js
@@ -0,0 +1,6 @@
+let cisDept = [ "Mike", "Clayton", "Terrill", "Danny", "Jennifer" ];
+let dmpDept = [ "Raymond", "Cynthia", "Bryan" ];
+let itDiv = cisDept.concat( dmpDept );
+console.log( itDiv );
+itDiv = dmpDept.concat( cisDept );
+console.log( itDiv );
