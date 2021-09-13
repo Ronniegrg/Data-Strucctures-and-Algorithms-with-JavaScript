25549c3e671e218534e74fb9083574f9d6f48ad3
Rouni Gorgees <rounigorgees@gmail.com>
2021-09-12 21:31:29 -0400

    splice() function creates a new array from the contents of an existing array.

---
+++ splice-function.js
@@ -0,0 +1,5 @@
+const itDiv = [ "Mike", "Clayton", "Terrill", "Raymond", "Cynthia", "Danny", "Jennifer" ];
+let dmpDept = itDiv.splice( 3, 3 );
+let cisDept = itDiv;
+console.log( dmpDept );
+console.log( cisDept );
