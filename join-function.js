dbaed98cc4bad1285dbe8ced72f8d46fbfc46ee0
Rouni Gorgees <rounigorgees@gmail.com>
2021-09-12 21:13:41 -0400

    use join function which returns string containing the lements of the array delimited by commas.

---
+++ join-function.js
@@ -0,0 +1,5 @@
+const names = [ "David", "Cynthia", "Raymond", "Clayton", "Mike", "Jennifer" ];
+let namestr = names.join();
+console.log( namestr );
+namestr = names.toString();
+console.log( namestr ); 
